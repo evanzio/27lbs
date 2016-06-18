b86d781792d1e8ed8f88a3aaeacbc0182f615889
Stephen Evans <00sevans@gmail.com>
2016-06-18 10:40:31 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+// var divID = document.getElementById('abc');
+// var dataID = divID.getAttribute('data-abc');
+
+// function change(arg) {
+// 	arg.setAttribute('data-abc', '99');
+// }
+
+
+// function splitMe(arg) {
+	
+// 	x = arg.split('');
+// 	console.log(x);
+
+// }
+
+//console.log(splitMe);
+
+
+// new site
+
+//Get element
+var logo__head = document.getElementById('logo__head');
+//Calculate width
+var logo__headHeight = logo__head.offsetWidth;
+//set attribute to value calculated
+logo__head.dataset.width = logo__headHeight
